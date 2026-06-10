a019e94b782452424d258a1a3f52c101739ce350
Zamira <zamiramontoro@gmail.com>
2026-06-10 20:33:56 -0300

    Guardar más ejercicios js

--- Javascript/Lección03/03-01-OperadoresJavaScript-UJS.js
+++ Javascript/Lección03/03-01-OperadoresJavaScript-UJS.js
@@ -25,3 +25,12 @@ if (dentroRango >= valMin && dentroRango <= valMax){
 else{
     console.log("Esta fuera del rango establecido")
 }
+
+// Ejercicio: si el padre puede asistir al juego de su hijo
+let vacaciones = true; diaDescanso = false; 
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego de su hijo")
+} 
+else{
+    console.log("El padre no puede asistir al juego de su hijo")
+}
