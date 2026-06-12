e7582bc006901d1bb8c8719779690872e58da895
Zamira <zamiramontoro@gmail.com>
2026-06-12 20:04:53 -0300

    Clase 7 Java Operadores de asignación

---
+++ JAVA/Leccion1/HolaMundo/src/CLASE07_OperadoresDeAsignación.java
@@ -0,0 +1,22 @@
+
+public class CLASE07_OperadoresDeAsignación {
+    public static void main(String[] args) {
+        
+        int varNum1 = 1, varNum2 =4;
+        var varNum3 = varNum1 + 6 - varNum2; //Una operación
+        System.out.println("varNum3 = " + varNum3);
+        
+        varNum1 += 1; //
+        System.out.println("varNum1 = " + varNum1);
+        
+        // -=  *=  /=  %=
+        varNum2 -= 2;
+        System.out.println("varNum2 = " + varNum2);
+        varNum1 *= 5;
+        System.out.println("varNum1 = " + varNum1);
+        varNum3 /= 4;
+        System.out.println("varNum3 = " + varNum3);
+        varNum1 %= 6;
+        System.out.println("varNum1 = " + varNum1);
+    }
+}
