1ce5df23ba01b8633827338aa4aa51c57521cec6
Zamira <zamiramontoro@gmail.com>
2026-06-12 19:44:46 -0300

    Ejercicio 1 Java Tienda de Libros

---
+++ JAVA/Leccion1/EJERCICIO_1_TiendaDeLibros/src/ejercicio_1_tiendadelibros/EJERCICIO_1_TiendaDeLibros.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_1_tiendadelibros;
+
+/**
+ *
+ * @author Zamira
+ */
+public class EJERCICIO_1_TiendaDeLibros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
