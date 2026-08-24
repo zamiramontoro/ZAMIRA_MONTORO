cc4c179d39182220dba24e736bcba18539d54abe
Zamira <zamiramontoro@gmail.com>
2026-08-24 20:57:14 -0300

    SS-PHYTON-2°CLASE-REPASO Y MÁS CONCEPTOS DE TUPLAS

--- 2° SEMESTRE/PROGRAMACIÓN II/Phyton/Lección4/CLASE1_LISTAS_PARTE_1_2_Y_3.py
+++ 2° SEMESTRE/PROGRAMACIÓN II/Phyton/Lección4/CLASE1_LISTAS_PARTE_1_2_Y_3.py
@@ -212,7 +212,13 @@ print(lista3)
 lista3.sort(reverse=True) # Ordena descendentemente
 print(lista3)
 
+# Repaso de Tuplas
+tupla = (4, "Hola", 6.78, [1, 2, 78], 4, "Hola") # Puede tener diferentes tipos de datos dentro
+print(tupla)
 
+print(4 in tupla) # Acción booleana, su respuesta es de tipo booleana
+# Lo que podemos usar dentro de tupplas son: index, count, len
+# En tuplas se puede convertir de tupla a lista y de lista a tupla
 
 
 
