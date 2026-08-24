51222dca4dd5060f68c9a4924ac2c685d6c4dfe4
Zamira <zamiramontoro@gmail.com>
2026-08-24 20:11:41 -0300

    SS-PHYTON-2°CLASE-DICCIONARIO

--- 2° SEMESTRE/PROGRAMACIÓN II/Phyton/Lección4/CLASE1_LISTAS_PARTE_1_2_Y_3.py
+++ 2° SEMESTRE/PROGRAMACIÓN II/Phyton/Lección4/CLASE1_LISTAS_PARTE_1_2_Y_3.py
@@ -114,7 +114,62 @@ print(planetas)
 
 #Eliminar set
 del planetas
-print(planetas) # al eliminar nos muestra un error
+# print(planetas) # al eliminar nos muestra un error
 
+# "Maradona":10 Un diccionario está compuesto por dos elementos
+# UNA LLAVE Y UN VALOR
+# dict(key,value)
+diccionario = {
+    "IDE":"Integrated Development Environment",
+    "POO":"Programación Orientada a Objetos",
+    "SABD":"Sistema de Administración de Base de Datos",
+}
+# Verificar la cantidad de elementos del diccionario
+print(len(diccionario))
+print(diccionario)
 
+# Acceder a un diccionario con la llave (key)
+print(diccionario["IDE"])
+
+# Otra forma de recuperar un elemento
+print(diccionario.get("POO"))
+print(diccionario.get("SABD"))
+
+# Modificamos los elementos
+diccionario["IDE"] = "Entorno de Desarrollo Integrado"
+print(diccionario)
+
+# Cómo recorrer los elementos
+for termino in diccionario: # Recorremos mostrando solo las llaves
+    print(termino)
+
+# Necesitamos una función para recorer un diccionario
+for termino, valor in diccionario.items():
+    print(termino, valor)
+
+# Otras maneras de acceder a un diccionario
+for termino in diccionario.keys(): # Estamos usando una función
+    print(termino) # Muestra solo las llaves
+
+for valor in diccionario.values(): # Usamos una función para acceder al valor
+    print(valor)
+
+# Comprobar la existencia de algún elemento
+print("IDE" in diccionario) # devuelve un booleano
+
+# Agregar un elemento
+diccionario["PK"] = "Primary key"
+print(diccionario)
+
+# Eliminar un elemento
+diccionario.pop("SABD")
+print(diccionario)
+
+# Vaciar un diccionario
+diccionario.clear()
+print(diccionario)
+
+# Eliminar diccionario
+del diccionario # El diccionario se borró
+print(diccionario)
 
